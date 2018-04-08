ae39eb6e4fd34ee5daf34988692a067f800ff998
Jonathan Earl <earl.jonathan@gmail.com>
2018-04-07 19:20:33 -0700

    setup get/post routes for Kitties

--- BackEnd-NoAuth/main.js
+++ BackEnd-NoAuth/main.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
-const app = express()
+const bodyParser = require('body-parser')
 
 // connect local DB
 mongoose.connect('mongodb://localhost/kg')
 
-const kittys = require('./routes/kittys')
-app.use('/kittys', kittys)
+let app = express()
+
+// set bodyPaser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// set view engine
+app.set('view engine', 'pug')
+
+const KittyRoute = require('./routes/KittiesRoute')
+app.use('/kitties', KittyRoute)
 
 app.listen('3000', () => {
     console.log('Server started on port 3000')
