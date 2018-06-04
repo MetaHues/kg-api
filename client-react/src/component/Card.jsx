e71d14e05cbfde71d9d0323d5fe7e2a42fb96f13
Jonathan Earl <wte@Jonathans-MacBook-Pro.local>
2018-06-03 17:14:53 -0700

    format cards

--- client-react/src/component/Card.jsx
+++ client-react/src/component/Card.jsx
@@ -22,19 +22,19 @@ class Card extends Component {
             <a className="bookmark_button"><i className="fa fa-bookmark-o"/></a>
           </div>
           <div className="section like_info">
-            <p>69 Grumpys</p>
+            <p><strong>69 Grumpys</strong></p>
           </div>
           <div className="section comments">
             <ol>
-              <li>Boom: This is a kitty, there are many like it, but this is mine. My kitty is my best friend. It is my life. I must master it as I must master my life. My kitty, without me, is useless. Without my kitty, I am useless. </li>
-              <li>Other1: comment 1</li>
-              <li>Other2: comment 2</li>
+              <li><strong>Boom:</strong> This is a kitty, there are many like it, but this is mine. My kitty is my best friend. It is my life. I must master it as I must master my life. My kitty, without me, is useless. Without my kitty, I am useless. </li>
+              <li><strong>Other1:</strong> comment 1</li>
+              <li><strong>Other2:</strong> comment 2</li>
             </ol>
           </div>
           <div className="section time_posted">69hours and 69mins ago</div>
           <div className="section comment_area">
-            <input type="text" name="" id=""/>
-            <button>...</button>
+            <input type="text" name="" id="" placeholder="Add a some glitter..."/>
+            <a><i className="fa fa-ellipsis-h"/></a>
           </div>
         </article>
     );
