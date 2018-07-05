f8a57d87eb7bcab5a89633231ed4e73ef273eb2b
jonathan earl <earl.jonathan@gmail.com>
2018-07-05 14:16:06 -0700

    fix log out

--- src/index.js
+++ src/index.js
@@ -11,15 +11,13 @@ const path = require('path')
 // Environmental variables
 const PORT = process.env.PORT || 5000
 
-// Configuration this should be hidden in environmental variables
-const config = require('./config/default')
-
 // Strategies
 const FacebookStrategy = require('./config/strategy').facebookStrategy
 passport.use(FacebookStrategy)
 
 // Passport and session setup
 // TODO: set in environmental variable not visible
+const config = require('./config/default')
 let uri = config.db.localUri
 if (process.env.NODE_ENV === 'production') uri = config.db.serverUri
 // connect DB with credentials
@@ -50,7 +48,12 @@ app.use('/api/post', PostRoute)
 const AuthRoute = require('./routes/Auth')
 app.use('/auth', AuthRoute)
 
-app.use(express.static(path.join(__dirname, '/../build')))
+console.log(path.join(__dirname, 'build'))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.use('*', (req,res) => {
+    res.sendfile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
