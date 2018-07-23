8eec29cd9202f5785bb43920b54c18789c7c5cae
jonathan earl <earl.jonathan@gmail.com>
2018-07-23 16:29:31 -0700

    add feature to edit bio msg

--- src/models/User.js
+++ src/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
     name: String,
     img: String,
     email: String,
+    msg: {
+        type: String,
+        default: null
+    },
     friends: [String],
     counts: {
         posts: {
--- src/routes/User.js
+++ src/routes/User.js
@@ -17,13 +17,40 @@ router.get('/', (req, res) => {
 })
 
 router.get('/profile', (req, res) => {
-    if(!req.user) {
+    if(!req.isAuthenticated) {
         res.statusMessage = 'NOT_AUTHORIZED'
-        res.sendStatus(403)
+        res.sendStatus(401)
     }
     let self = Object.assign({}, req.user._doc)
     self.isAuthenticated = true
     res.json(self)
+})
+
+router.put('/profile', (req, res) => {
+    if(!req.isAuthenticated) {
+        res.statusMessage = 'NOT_AUTHORIZED'
+        res.sendStatus(401)
+    }
+    if(req.body.msg.length > 150) {
+        res.statusMessage = 'MSG_TOO_LONG'
+        res.sendStatus(400)
+    }
+
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {$set: {msg: req.body.msg}},
+        {new: true}
+    )
+    .then(updatedSelf => {
+        let self = Object.assign({}, updatedSelf._doc)
+        self.isAuthenticated = true;
+        res.json({success: true, self: self})
+    })
+    .catch(err => {
+        console.log(err)
+        res.statusCode = 500
+        res.json({success: false, message: 'INTERNAL_ERROR'})
+    })
 })
 
 router.get('/:userId', (req, res) => {
@@ -35,23 +62,25 @@ router.get('/:userId', (req, res) => {
 
     User.findById(req.params.userId)
     .then(theUser => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(theUser))
+        res.json(theUser._doc)
     })
     .catch(err => {
+        res.statusCode = 404
+        res.json({success: false, message: 'NO_USER'})
         console.log("error " + err)
     })
 })
 
 router.post('/', (req, res) => {
     let newUser = new User(req.body);
-    newUser
-    .save()
-    .then(() => {
-        res.send('success')
+    newUser.save()
+    .then(createdUser => {
+        res.json({success: false, user: createdUser})
     })
     .catch(() => {
-        res.send('error')
+        res.statusCode = 500
+        console.log("error " + err)
+        res.json({success: false, message: 'INTERNAL_ERROR'})
     })
 })
 
