88786ee56e77bd627aa7ecfd9cb445b5e61f2c32
Jonathan Earl <earl.jonathan@gmail.com>
2018-08-24 12:41:43 -0700

    fix any issues

--- src/config/strategy.js
+++ src/config/strategy.js
@@ -12,20 +12,15 @@ const s3 = new AWS.S3({})
 
 
 const uploadToS3 = (userId, profileImgUrl) => {
-    console.log('profile', `${String(userId)}.jpg`)
     const key = urljoin('profile', `${userId}.jpg`)
-    console.log('key', key)
     let requestOptions = {
         url: profileImgUrl,
         responseType:'stream'
     }
-    axios(requestOptions)
+    return axios(requestOptions)
     .then(res => {
         const params = {Bucket: process.env.AWS_BUCKET, Key: key, Body: res.data}
-        s3.upload(params, (err, data) => {
-            if(err) console.log(err)
-            else console.log('data', data)
-        })
+        return s3.upload(params).promise()
     })
 }
 
@@ -38,28 +33,33 @@ module.exports = {
         profileFields: ['id', 'displayName', 'email', 'picture.type(large)']
     },
     function(accessToken, refreshToken, profile, done) {
+        let user = {}
         User.findOne({'facebook.id': profile.id})
         .then(existingUser => {
             if(existingUser) {
-                return done(null, existingUser)
-            } else {
-                new User({
-                    facebook: {
-                        id: profile.id,
-                    },
-                    name: profile.displayName,
-                    email: profile.emails[0].value,
-                }) 
-                .save()                         // save new user
-                .then(newUser => {
-                    const fbProfileImgUrl = profile.photos[0].value
-                    uploadToS3(String(newUser._id), fbProfileImgUrl)
-                    return done(null, newUser)  // return new user to be sent back 
-                })
-                .catch(err => {
-                    return done(err)            // pass error on
-                })
+                done(null, existingUser)
+                throw new Error('user already exists')
             }
+            // create new user
+            return new User({
+                facebook: {
+                    id: profile.id,
+                },
+                name: profile.displayName,
+                email: profile.emails[0].value,
+            }).save()                         
+        })
+        .then(newUser => {
+            user = newUser
+            // upload photo to s3
+            const fbProfileImgUrl = profile.photos[0].value
+            return uploadToS3(String(newUser._id), fbProfileImgUrl)
+        }).then(res => {
+            user.img = res.Location
+            return user.save({new: true})
+        })
+        .then(updatedUser => {
+            done(null, updatedUser)
         })
         .catch((err) => {
             return done(err)
