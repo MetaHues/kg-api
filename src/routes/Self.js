9effff2cf03218dc765ec6e2993849802f598589
jonathan earl <earl.jonathan@gmail.com>
2018-09-18 16:20:56 -0700

    modify recent activity to send posts and user info along with activities

--- src/routes/Self.js
+++ src/routes/Self.js
@@ -4,6 +4,8 @@ const ObjectId = require('mongoose').Types.ObjectId
 // import model
 const Like = require('../models/Like')
 const Comment = require('../models/Comment')
+const User = require('../models/User')
+const Post = require('../models/Post')
 // const Friend = require('../models/Friend')
 
 // Self Routes for retrieving recent information posted at the user
@@ -27,7 +29,23 @@ router.get('/RecentActivity', async(req, res) => {
     try {
         let comments = await Comment.find({userId: {$ne: req.user._id}, postOwnerId: req.user._id})
         let likes = await Like.find({userId: {$ne: req.user._id}, postOwnerId: req.user._id})
-        return res.json({likes, comments})
+        // get all posts and users in recent activity
+        // get all postId userId
+        let userIds = []
+        let postIds = []
+        comments.forEach(comment => {
+            userIds.push(comment.userId)
+            postIds.push(comment.postId)
+        })
+        likes.forEach(like => {
+            userIds.push(like.userId)
+            postIds.push(like.postId)
+        })
+        // retrieve user and post by id
+        let users = await User.find({_id: {$in: userIds}})
+        let posts = await Post.find({_id: {$in: postIds}})
+        // send to user
+        return res.json({likes, comments, users, posts})
     } catch(err) {
         console.log("error " + err)
         res.statusCode = 501
